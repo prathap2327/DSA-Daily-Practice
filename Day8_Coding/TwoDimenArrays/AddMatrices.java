4138c20014f5201bccd9958d21e69aa0cc27595a
Ram <ramprathap221@gmail.com>
2026-04-11 22:31:55 +0530

    ADD MAtrices

---
+++ Day8_Coding/TwoDimenArrays/AddMatrices.java
@@ -0,0 +1,39 @@
+package Day8_Coding.TwoDimenArrays;
+
+public class AddMatrices {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int[][] matrix2 = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+        int[][] sum = addMatrices(matrix1, matrix2);
+        printMatrix(sum);
+    }
+
+    public static int[][] addMatrices(int[][] a, int[][] b) {
+        int rows = a.length;
+        int cols = a[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int elem : row) {
+                System.out.print(elem + " ");
+            }
+            System.out.println();
+        }
+    }
+}
