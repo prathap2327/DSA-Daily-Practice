f6cb5cf00a102c4bba35467f05f31e140ad1807a
Ram <ramprathap221@gmail.com>
2026-04-15 23:12:30 +0530

    Adding elements to AL

---
+++ Day8_Coding/ArrayList/AddelementstoAL.java
@@ -0,0 +1,13 @@
+package Day8_Coding.ArrayList;
+
+import java.util.ArrayList;
+
+public class AddelementstoAL {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        System.out.println(numbers);
+    }
+}
