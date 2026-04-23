8ee310fb6c1bba9c592819970b7886967c5552e4
Ram <ramprathap221@gmail.com>
2026-04-23 23:25:11 +0530

    Palindrome or not

---
+++ Strings/StringPalindromeorNot.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class StringPalindromeorNot {
+
+    public static boolean isPalindrome(String str)
+    {
+        int left = 0;
+        int right = str.length() - 1;
+        while(left < right)
+        {
+            if(str.charAt(left) != str.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+        System.out.println("Is the string a palindrome? " + isPalindrome(str));
+    }
+}
