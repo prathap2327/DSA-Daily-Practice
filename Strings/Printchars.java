c42bc0dc455f4064f6f9494183a401b822b4e2df
Ram <ramprathap221@gmail.com>
2026-04-23 19:24:22 +0530

    Print chars in new Line

---
+++ Strings/Printchars.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Printchars {
+    public static void main(String[] args) {
+		String country = "INDIA";
+		for(int i=0;i<country.length();i++)
+		{
+		System.out.println(country.charAt(i));
+		}
+	}
+}
