0f4a101760bb15ef8b7db8c9ae7ddd9067527c36
Ram <ramprathap221@gmail.com>
2026-05-23 22:19:18 +0530

    changes

--- Streams/SumNumsReduce.java
+++ Streams/SumNumsReduce.java
@@ -1,5 +1,8 @@
 package Streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SumNumsReduce {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
