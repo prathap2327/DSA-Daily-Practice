5297d81c218d226137ad5425cae88522782ae147
Ram <ramprathap221@gmail.com>
2026-05-25 10:55:45 +0530

    space optimised

--- prefixSum/ProductArrayOptimise.java
+++ prefixSum/ProductArrayOptimise.java
@@ -3,25 +3,42 @@ package prefixSum;
 import java.util.Arrays;
 
 public class ProductArrayOptimise {
-    public static int[] ProdArray(int[] nums)
+    // public static int[] ProdArray(int[] nums)
+    // {
+    //     int N = nums.length;
+    //     int[] pf = new int[N];
+    //     int[] sf = new int[N];
+    //     int[] ans = new int[N];
+    //     pf[0] = 1;
+    //     for(int i=1;i<N;i++)
+    //     {
+    //         pf[i] = pf[i-1] * nums[i-1];
+    //     }
+    //     sf[N-1]=1;
+    //     for(int i=N-2;i>=0;i--)
+    //     {
+    //         sf[i] = sf[i+1] * nums[i+1];
+    //     }
+    //     for(int i=0;i<N;i++)
+    //     {
+    //         ans[i] = pf[i] * sf[i];
+    //     }
+    //     return ans;
+    // }
+     public static int[] ProdArray(int[] nums)
     {
         int N = nums.length;
-        int[] pf = new int[N];
-        int[] sf = new int[N];
         int[] ans = new int[N];
-        pf[0] = 1;
+        ans[0] = 1;
         for(int i=1;i<N;i++)
         {
-            pf[i] = pf[i-1] * nums[i-1];
+            ans[i] = ans[i-1] * nums[i-1];
         }
-        sf[N-1]=1;
-        for(int i=N-2;i>=0;i--)
+        int right=1;
+        for(int i=N-1;i>=0;i--)
         {
-            sf[i] = sf[i+1] * nums[i+1];
-        }
-        for(int i=0;i<N;i++)
-        {
-            ans[i] = pf[i] * sf[i];
+            ans[i] = ans[i]*right;
+            right = right*nums[i];
         }
         return ans;
     }
