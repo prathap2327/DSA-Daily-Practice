4a373b2f03b9549fa218cd009b4731080f5cb68e
Ram <ramprathap221@gmail.com>
2026-06-19 15:15:47 +0530

    maxSubarray

---
+++ slidingWindow/maxSubbary.java
@@ -0,0 +1,29 @@
+package slidingWindow;
+
+public class maxSubbary {
+    public static int maxSubarray(int A, int B, int[] C) {
+        int sum =0;
+	    int start=0;
+	    int max =0;
+	    for(int i=0;i<A;i++)
+	    {
+	        sum += C[i];
+	        while(sum>B)
+	        {
+	            sum -= C[start];
+	            start++;
+	        }
+	        max = Math.max(sum,max);
+	    }
+    
+    return max;
+    
+}
+public static void main(String[] args) {
+    int A = 5;
+	    int B = 12;
+	    int[] C = {2, 1, 3, 4, 5};
+    System.out.println(maxSubbary.maxSubarray(A, B, C));
+}
+}
+
